12a422fffaf56f97d2ff37a0c996fc5c7ebd4f49
hellison.oliveira <hellison.oliveira@gmail.com>
2017-06-20 23:19:35 -0300

    lesson 06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-  hero = 'Windstorm'
+    title = 'Olá Mundo!';
 }
--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[] = ['PHP', 'Java', 'Angular', 'AWS'];
+
+  constructor() {
+      this.nomePortal = "https://hellinenathy.herokuapp.com/index.html"
+
+      for (let i=0; i<this.cursos.length; i++){
+        let curso = this.cursos[i];
+      }
+   }
 
   ngOnInit() {
   }
