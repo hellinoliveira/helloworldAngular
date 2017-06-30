893f4b577aa989fee5960cbf5fbaa078c3be2bbe
hellison.oliveira <hellison.oliveira@gmail.com>
2017-06-30 17:54:45 -0300

    databinding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -3,23 +3,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  // styleUrls: ['./data-binding.component.css']
+  styles: [` .highlight {
+    background-color: pink
+  } `]
 })
 export class DataBindingComponent implements OnInit {
 
   url: string = "www.helldev.com"
   urlImage: string = "http://lorempixel.com/200/100/nature/"
   angularCourse: boolean = true;
-  getValue(){
+
+  oldValue: string = "";
+  newValue: string = "";
+
+  isMouseOver: boolean = false;
+  getValue() {
     return 1;
   }
 
-  getLikeAngular(){
+  getLikeAngular() {
     return true;
   }
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clickedButton() {
+    console.log('Hello!')
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.oldValue = (<HTMLInputElement>event.target).value;
+  }
+
+  saveNewValue(value) {
+    this.newValue = value;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
 }
